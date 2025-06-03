1d92a080cbe3d578e5940cab0b577fd56b577e7a
Gulla Maneesh <gullamanish2045@gmail.com>
2025-06-03 13:10:14 +0530

    completed theme switcher

---
+++ themeSwitcher/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // Move darkMode here from vite.config.js
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
